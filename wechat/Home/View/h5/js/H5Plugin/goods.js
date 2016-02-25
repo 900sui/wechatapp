25d471584758ae43da3a117627c157d74c9cddbd
shenjunqing <100776613@qq.com>
2016-02-25 14:02:24 +0800

    html5 for app

---
+++ wechat/Home/View/h5/js/H5Plugin/goods.js
@@ -0,0 +1,54 @@
+document.addEventListener( "plusready",  plusReady, true );
+//document.addEventListener( "onload",  onLoad, false );
+
+function plusReady(){
+	//document.addEventListener( "onload",  onLoad, false );
+
+	var _BARCODE = 'plugintest',
+		B = window.plus.bridge;
+	var plugintest =
+	{
+		PluginGoodsFunction : function (Argus1,  successCallback, errorCallback )
+		{
+			var success = typeof successCallback !== 'function' ? null : function(args)
+				{
+					successCallback(args);
+				},
+				fail = typeof errorCallback !== 'function' ? null : function(code)
+				{
+					errorCallback(code);
+				};
+			callbackID = B.callbackId(success, fail);
+
+			return B.exec(_BARCODE, "PluginGoodsFunction", [callbackID, Argus1]);
+		},
+		PluginGetCityIdFunction : function (Argus1,  successCallback, errorCallback )
+		{
+			//alert("PluginGetCityIdFunction");
+			var success = typeof successCallback !== 'function' ? null : function(args)
+				{
+					successCallback(args);
+				},
+				fail = typeof errorCallback !== 'function' ? null : function(code)
+				{
+					errorCallback(code);
+				};
+			callbackID = B.callbackId(success, fail);
+
+			return B.exec(_BARCODE, "PluginGetCityIdFunction", [callbackID,Argus1]);
+		}
+	};
+
+
+	window.plus.plugintest = plugintest;
+
+}
+
+
+
+function getCommondGoods(){
+	$.get("http://jkd2.shutung.com:81/App/v3/Ad/commendGoods",function(res){
+		alert(json.total);
+	},"jsonp");
+}
+
