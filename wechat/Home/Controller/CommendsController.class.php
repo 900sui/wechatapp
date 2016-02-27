51c26d453c873cf05e035476b964d6643d3c5aef
shenjunqing <100776613@qq.com>
2016-02-27 17:46:11 +0800

    html5 for app

--- wechat/Home/Controller/CommendsController.class.php
+++ wechat/Home/Controller/CommendsController.class.php
@@ -30,7 +30,7 @@ class CommendsController extends Controller
 
         $result = json_decode($response);
         //dump($result);
-        $bus = $result->data;
+        $bus = $result;
         //dump(count($bus));
         $container = "<div class=\"row x-row x-menu2\" >";
         if ($result->error == 0) {
@@ -70,79 +70,30 @@ class CommendsController extends Controller
         $u_result = json_decode($response);
         $u = $u_result->data;
         //dump($u);
-        $u_container = "<div class=\"row x-row x-menu2\" >";
-        if ($u_result->error == 0) {
-            for ($i = 0; $i < count($u); $i++) {
-                $u_container .= "<div class=\"col col-xs-4\" ><img name='"
-                    . $u[$i]->type
-                    . "_" . $u[$i]->target_id
-                    . "'  src=" . $u[$i]->pic_path
-                    . " class=\"img-responsive\"/></div>";
-                //dump(($i + 1) % 3 == 0);
-                if (($i + 1) % 3 == 0 && ($i + 1) < count($u)) {
-                    $u_container .= "</div><div class=\"row x-row x-menu2\" >";
-                }
-            }
-            $u_container .= "</div>";
-            //dump($container);
-            $this->assign("u_container", $u_container);
 
-        } else {
-            //echo $result->errorMsg;
-            $this->assign("bus_container",  $u_result->errorMsg);
+        if ($u_result->error == 0) {
+            //dump($container);
+            $this->assign("u_container", $u);
 
         }
         //推荐商户
         $response = $ch->http($busUrl, $data);
         $bus_result = json_decode($response);
         $bus = $bus_result->data;
-        $bus_container = "<div class=\"row x-row x-menu2\" >";
         if ($bus_result->error == 0) {
-            for ($i = 0; $i < count($bus); $i++) {
-                $bus_container .= "<div class=\"col col-xs-4\" ><img name='"
-                    . $bus[$i]->type
-                    . "_" . $bus[$i]->target_id
-                    . "'  src=" . $bus[$i]->pic_path
-                    . " class=\"img-responsive\"/></div>";
-                //dump(($i + 1) % 3 == 0);
-                if (($i + 1) % 3 == 0 && ($i + 1) < count($bus)) {
-                    $bus_container .= "</div><div  class=\"row x-row x-menu2\" >";
-                }
-            }
-            $bus_container .= "</div>";
             //dump($container);
-            $this->assign("bus_container", $bus_container);
-
-        } else {
-            //echo $result->errorMsg;
-            $this->assign("bus_container",  $bus_result->errorMsg);
-
+            $this->assign("bus_container", $bus);
         }
 
         //推荐产品
         $response = $ch->http($goodsUrl);
         $goods_result = json_decode($response);
         $goods = $goods_result->data;
-        $goods_container = "<div class=\"row x-row x-menu2\" >";
-        if ($goods_result->error == 0) {
-            for ($i = 0; $i < count($goods); $i++) {
-                $goods_container .= "<div class=\"col col-xs-4\" ><img name='"
-                    . $goods[$i]->type
-                    . "_" . $goods[$i]->target_id
-                    . "'  src=" . $goods[$i]->pic_path
-                    . " class=\"img-responsive\"/></div>";
-                //dump(($i + 1) % 3 == 0);
-                if (($i + 1) % 3 == 0 && ($i + 1) < count($goods)) {
-                    $goods_container .= "</div><div  class=\"row x-row x-menu2\" >";
-                }
-            }
-            $goods_container .= "</div>";
-            //dump($container);
-            $this->assign("goods_container", $goods_container);
 
-        } else {
-            //echo $result->errorMsg;
-            $this->assign("goods_container",  $goods_result->errorMsg);
+        if ($goods_result->error == 0) {
+
+            //dump($container);
+            $this->assign("goods_container", $goods);
 
         }
 
@@ -150,26 +101,12 @@ class CommendsController extends Controller
         $response = $ch->http($safeUrl);
         $safe_result = json_decode($response);
         $safe = $safe_result->data;
-        $safe_container = "<div class=\"row x-row x-menu2\" >";
-        if ($safe_result->error == 0) {
-            for ($i = 0; $i < count($safe); $i++) {
-                $safe_container .= "<div class=\"col col-xs-4\" ><img name='"
-                    . $safe[$i]->type
-                    . "_" . $safe[$i]->target_id
-                    . "'  src=" . $safe[$i]->pic_path
-                    . " class=\"img-responsive\"/></div>";
-                //dump(($i + 1) % 3 == 0);
-                if (($i + 1) % 3 == 0 && ($i + 1) < count($safe)) {
-                    $safe_container .= "</div><div  class=\"row x-row x-menu2\" >";
-                }
-            }
-            $safe_container .= "</div>";
-            //dump($container);
-            $this->assign("safe_container", $safe_container);
 
-        } else {
-            //echo $result->errorMsg;
-            $this->assign("safe_container",  $safe_result->errorMsg);
+        //dump($safe);
+        if ($safe_result->error == 0) {
+
+            //dump($container);
+            $this->assign("safe_container", $safe);
 
         }
 
